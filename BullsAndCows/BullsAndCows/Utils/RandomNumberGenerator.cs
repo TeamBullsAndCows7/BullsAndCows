26dd84bee0013e6f645a64b99e982f469b4ee63d
mitko100 <dimitar.radenkov1@gmai.com>
2014-07-20 10:34:39 +0300

    RandomNumberGenerator - added. ScoreBoard class added - will hold players scores.

---
+++ BullsAndCows/BullsAndCows/Utils/RandomNumberGenerator.cs
@@ -0,0 +1,46 @@
+﻿namespace BullsAndCows.Utils
+{
+    using System;
+    using System.Text;
+
+    public class RandomNumberGenerator
+    {
+        private static RandomNumberGenerator instance;
+        private Random random;
+        private const int NumberLength = 4;
+        private const int MinNumber = 0;
+        private const int MaxNumber = 9;
+
+        private RandomNumberGenerator() 
+        {
+            this.random = new Random();
+        }
+
+        public static RandomNumberGenerator Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new RandomNumberGenerator();
+                }
+                return instance;
+            }
+        }
+
+        public int Next()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < NumberLength; i++)
+            {
+                int randomDigit = this.random.Next(MinNumber, MaxNumber);
+                sb.Append(randomDigit);
+            }
+
+            int generatedNumber = int.Parse(sb.ToString());
+
+            return generatedNumber;
+        }
+    }
+}
---
+++ BullsAndCows/BullsAndCows/Utils/ScoreBoard.cs
@@ -0,0 +1,42 @@
+﻿namespace BullsAndCows.Utils
+{
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class ScoreBoard
+    {
+        private const int MaxScoresNumber = 5;
+        private SortedDictionary<int, string> scoresList;
+
+        public ScoreBoard()
+        {
+            this.scoresList = new SortedDictionary<int, string>();
+        }
+
+        public void Enter(int score, string player)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            if (this.scoresList.Count == 0)
+            {
+                sb.Append("\n-- Score Board is Empty --\n");
+            }
+            else
+            {
+                int rank = 1;
+                foreach (var item in this.scoresList)
+                {
+                    sb.AppendFormat("{0}. {1} --> {2} guesses", rank, item.Key, item.Value);
+                    ++rank;
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
