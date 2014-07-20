ec3af958fd04c4f0b5aa7629febfff4244633a22
mitko100 <dimitar.radenkov1@gmai.com>
2014-07-20 18:36:42 +0300

    Game logic is separated in other class.

--- BullsAndCows/BullsAndCows/Driver/Driver.cs
+++ BullsAndCows/BullsAndCows/Driver/Driver.cs
@@ -1,48 +1,54 @@
 ﻿namespace BullsAndCows.Driver
 {
     using BullsAndCows.Commands;
+    using BullsAndCows.Logic;
     using BullsAndCows.Utils;
     using System;
 
     public class Driver : IDriver
     {
-        private ScoreBoard scoreBoard;
-        private RandomNumberGenerator randomNumberGenerator;
-        private ICommand currentCommand;
-        private int secretNumber;
+        private ILogic gameLogic;
 
         public Driver()
         {
-            this.scoreBoard = new ScoreBoard();
-            this.randomNumberGenerator = RandomNumberGenerator.Instance;
-            this.secretNumber = this.randomNumberGenerator.Next();
+            this.gameLogic = new NormalLogic();
         }
 
         public void Start()
         {
-            Console.WriteLine("Welcome to “Bulls and Cows” game.\nPlease try to guess my secret 4-digit number.\n\n");
+            Console.WriteLine("Welcome to “Bulls and Cows” game.\nPlease try to guess my secret 4-digit number.\n");
             Console.WriteLine("Use one of the following command: ");
             Console.WriteLine("\r'top' - view the top scoreboard.");
             Console.WriteLine("\r'restart' - start a new game.");
             Console.WriteLine("\r'help' - reveal a number.");
             Console.WriteLine("\r'exit' - quit the game.");
 
+            while (this.gameLogic.Run)
+            {
+                Console.Write("Enter your guess or command: ");
+                string userInput = Console.ReadLine();
 
+                try
+                {
+                    ICommand command = CommandFactory.Create(userInput);
+                    command.Execute();    
+                }
+                catch (ArgumentException argumentExc)
+                {
+                    Console.WriteLine(argumentExc.Message);
+                }           
+            }
         }
 
         public void Stop()
         {
-
+                    
         }
 
         public void Restart()
         {
-            this.secretNumber = randomNumberGenerator.Next();
-        }
-
-        private void ReadUserInput()
-        {
-            
+            this.Stop();
+            this.Start();
         }
     }
 }
