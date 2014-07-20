3c6247baf95d3979aab20db5b7c59661ba3c2f1b
mitko100 <dimitar.radenkov1@gmai.com>
2014-07-20 18:39:29 +0300

    CommandFactory class - imlemented.

---
+++ BullsAndCows/BullsAndCows/Commands/CommandFactory.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BullsAndCows.Commands
+{
+    public static class CommandFactory
+    {
+        public static ICommand Create(string input)
+        {
+            ICommand command = null;
+
+            switch (input)
+            {
+                case "help":
+                    {
+                        command = new HelpCommand();
+                        break;
+                    }
+                case "top":
+                    {
+                        command = new TopCommand();
+                        break;
+                    }
+                case "restart":
+                    {
+                        command = new RestartCommand();
+                        break;
+                    }
+                case "exit":
+                    {
+                        command = new ExitCommand();
+                        break;
+                    }
+                default:
+                    {
+                        if (input.Length == 4)
+                        {
+                            int userNumber;
+                            if (int.TryParse(input, out userNumber))
+                            {
+                                command = new GuessNumberCommand();
+                                break;
+                            }
+                            else
+                            {
+                                throw new ArgumentException("Wrong command, please try again.");
+                            }   
+                        }
+                        else
+                        {
+                            throw new ArgumentException("Wrong number, please try again.");
+                        } 
+                    }      
+            }
+
+            return command;
+        }
+    }
+}
