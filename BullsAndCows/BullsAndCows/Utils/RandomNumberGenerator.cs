da977c0782f0cee711e27dd1db7eb4f9311195cb
mitko <dimitar.radenkov1@gmail.com>
2014-07-25 14:34:53 +0300

    generating random number logic changed. Now generates random number between 1000 and 9999.

--- BullsAndCows/BullsAndCows/Utils/RandomNumberGenerator.cs
+++ BullsAndCows/BullsAndCows/Utils/RandomNumberGenerator.cs
@@ -6,10 +6,9 @@
     public class RandomNumberGenerator
     {
         private static RandomNumberGenerator instance;
-        private readonly Random random;
-        private const int NumberLength = 4;
-        private const int MinNumber = 0;
-        private const int MaxNumber = 9;
+        private readonly Random random;     
+        private const int MinNumber = 1000;
+        private const int MaxNumber = 9999;
 
         private RandomNumberGenerator() 
         {
@@ -30,28 +29,7 @@
 
         public int Next()
         {
-            StringBuilder sb = new StringBuilder();
-
-            for (int i = 0; i < NumberLength; i++)
-            {
-                int randomDigit = this.random.Next(MinNumber, MaxNumber + 1);
-
-                if (i == 0 && randomDigit == 0)
-                {
-                    randomDigit++;
-                }
-
-                while (sb.ToString().IndexOf(randomDigit.ToString()) >= 0)
-                {
-                    randomDigit = this.random.Next(MinNumber, MaxNumber + 1);
-                }
-
-                sb.Append(randomDigit);
-            }
-
-            int generatedNumber = int.Parse(sb.ToString());
-
-            return generatedNumber;
+            return this.random.Next(MinNumber, MaxNumber);
         }
     }
 }
