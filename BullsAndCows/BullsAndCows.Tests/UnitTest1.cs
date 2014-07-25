c4e27929671acf9603980a1415e31949e1452843
mitko <dimitar.radenkov1@gmail.com>
2014-07-25 15:20:51 +0300

    score board unit tests - added.

--- BullsAndCows/BullsAndCows.Tests/UnitTest1.cs
+++ BullsAndCows/BullsAndCows.Tests/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BullsAndCows.Utils;
 
 namespace BullsAndCows.Tests
 {
@@ -15,6 +16,38 @@ namespace BullsAndCows.Tests
         public void TestMetod2()
         {
            
+        }
+
+        [TestMethod]
+        public void TestScoreBoardSeveralPlayersWithSameScore()
+        {
+            ScoreBoard sc = new ScoreBoard();
+
+            sc.Enter(1, "Pesho");
+            sc.Enter(2, "Mitko");
+            sc.Enter(2, "Dimitar");
+
+            string actual = sc.ToString();
+            string expected = "\nTop Score Board:\n1. Pesho --> 1 guesses\n2. Mitko --> 2 guesses\n2. Dimitar --> 2 guesses\n";
+
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+        public void TestScoreBoardEnterMethod()
+        {
+            ScoreBoard sc = new ScoreBoard();
+
+            sc.Enter(1, "Mitko");
+            sc.Enter(2, "Niki");
+            sc.Enter(3, "Conko");
+            sc.Enter(4, "Viki");
+            sc.Enter(5, "Viki");
+
+            bool actual = sc.HasToSaveScore(6);
+            bool expected = false;
+
+            Assert.AreEqual(expected, actual);
         }
     }
 }
